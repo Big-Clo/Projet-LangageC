b752f781b67df9a64a1d2c836092fe7fdd5580a9
Big-Clo <claudeandiaye06@gmail.com>
2025-07-08 22:51:36 +0000

    en cours

---
+++ Test1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int claude = 34;
+    printf("Premiere valeur : %d",claude);
+    scanf("%d",&claude);
+    printf("Deuxieme valeur : %d",claude);
+    return 0;
+}
