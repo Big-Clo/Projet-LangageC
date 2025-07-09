c9ee32351fcd5ff0a1b2f7900a7e31e849d98fe6
kahmancoder <oka069242gmail.com>
2025-07-09 12:51:09 +0000

    biblio_classe

--- menuClasse.c
+++ menuClasse.c
@@ -5,19 +5,23 @@
 void menuClasse()
 {
     int choix, a;
-    printf("Bienvenue dans notre programme.\n
-        Pour ajouter un classe tapez 1, \n
-        Pour afficher la liste des classe tapez 2\n");
+
+    do {
+    printf("\t\tBienvenue dans notre programme.\n");
+    printf("1. Pour ajouter un classe\n");
+    printf("2. Pour afficher la liste des classe\n");
+    printf("0. Pour quitter\n");
+    printf("Renseigner votre choix\n");
     scanf("%d", &choix);
+
     switch (choix)
     {
     case 1:
         a = ajout_classe();
         if (a ==0)
-            printf("\n L'ajout a reussi");
-            
+            printf("\n L'ajout a reussi\n"); 
         else
-            printf ("\nL'ajout a echoue");
+            printf ("\nL'ajout a echoue\n");
             break;
     case 2:
         printf("Voici la liste des classes\n");
@@ -25,8 +29,11 @@ void menuClasse()
         break;
     
     default:
-        printf("Nous ne pouvons pas encore gerer cette option");
+        printf("Nous ne pouvons pas encore gerer cette option\n");
         break;
     }
     
+    
+    } while (choix !=0);
+
 }
