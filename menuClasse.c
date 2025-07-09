1f7e83d273ad164073d4589972ea79833ee1cd22
kahmancoder <oka069242gmail.com>
2025-07-09 12:26:00 +0000

    menu classe

---
+++ menuClasse.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "gestion_classe.h"
+#include <stdlib.h>
+
+void menuClasse()
+{
+    int choix, a;
+    printf("Bienvenue dans notre programme.\n
+        Pour ajouter un classe tapez 1, \n
+        Pour afficher la liste des classe tapez 2\n");
+    scanf("%d", &choix);
+    switch (choix)
+    {
+    case 1:
+        a = ajout_classe();
+        if (a ==0)
+            printf("\n L'ajout a reussi");
+            
+        else
+            printf ("\nL'ajout a echoue");
+            break;
+    case 2:
+        printf("Voici la liste des classes\n");
+        afficher_classe();
+        break;
+    
+    default:
+        printf("Nous ne pouvons pas encore gerer cette option");
+        break;
+    }
+    
+}
