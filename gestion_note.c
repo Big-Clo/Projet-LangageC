d80b09e18edf755d678579456241a2a34c177205
Ousmae Ladetail <dieyeousmanedieng@gmail.com>
2025-07-10 19:52:19 +0000

    ajout gestion notes

--- gestion_note.c
+++ gestion_note.c
@@ -1,78 +1,70 @@
-#include "gestion_note.h"
-#include "etudiant.h"
 #include <stdio.h>
-// int numero;
-//     char nom[30];
-//     char prenom[30];
-//     char email[50];
-//     Date date_naissance;
-//     int codeClasse;
+#include "gestion_note.h"
+#include "etudiant.c"
 
-// int numero_existe(int x){
-//     char nom[30], prenom[30], email[50];
-//     Date date_naissance;
-//     int numero, codeClasse;
-//     FILE  *fichier_classe = 
-//     fopen ("etudiant.csv", "r");
-//     while (fscanf(fichier_classe, "%d - %s - %s\n", &code, nom, niveau) != -1){
-//         if (x == code){
-//             fclose(fichier_classe);
-//             return 1;
+
+
+
+int numero_existe(int x){
+    char nom[30], prenom[30], email[50];
+    int numero, codeClasse;
+    Date date_naissance;
+    FILE  *fichier_etudiants = fopen ("etudiants.csv", "r");
+    while (fscanf(fichier_etudiants, "%d - %s - %s - %s - %d / %d / %d - %d\n", &numero, nom, prenom, email, &date_naissance.jour, &date_naissance.mois, &date_naissance.annee, &codeClasse) != -1){
+        if (x == numero){
+            fclose(fichier_etudiants);
+            return 1;
             
-//         }
-//     }
-//     fclose(fichier_classe);
-//     return 0;
-// }
+        }
+    }
+    fclose(fichier_etudiants);
 
-// int ajout_note(){
-//     NOTE note;
-//     int level, existe;
-    
-//     printf ("Ce programme vous permet d'ajouter une note a un eleve.\n Veuillez saisir:\n le numero de l'etudiant:\t");
-//     scanf("%d", &note.numero);
-//     existe = code_existe(classe.code);
-//     while(existe == 1 ){
-//         printf ("Le code que vous avez saisit est deja attribue a une classe veuillez saisir un autre code ou -1 si vous voulez annuler l'ajout:\t");
-//         scanf("%d", &classe.code);
-//         existe = code_existe(classe.code);
-//         if (classe.code == -1){
-//             break;
-//         }
-//     }
-    
-//     if (classe.code == -1){
-//         printf("L'ajout a ete annule");
-//         return 1;
-//     }
-//     printf ("\n Le nom de la classe:\t\n");
-//     scanf ("%s", classe.nom);
-//     printf ("\nVeuillez tapez 1 si vous etes en license et 2 si vous etes en master:\t");
-//     scanf("%d", &level);
-//     while (level != 1 && level != 2){
-//         printf("\nVous devez entrer 1 si vous etes en license et 2 si vous etes en master. Veuillez reessayer:\t");
-//         scanf("%d", &level);
-//     }
-//     if (level == 1){
-//         classe.niveau = License;
-//     }
-//     else{
-//         classe.niveau = Master;
-//     }
+    return 0;
+}
+int reference_existe(int x){
+    char libelle[30];
+    int reference, coefficient;
+    FILE  *fichier_matiere = fopen ("matiere.csv", "r");
+    while (fscanf(fichier_matiere, "%d - %s - %d\n", &reference, libelle, coefficient ) != -1){
+        if (x == reference){
+            fclose(fichier_matiere);
+            return 1;
+            
+        }
+    }
+    fclose(fichier_matiere);
 
-//     FILE  *fichier_classe = fopen ("classe.csv", "a");
-//     if (fichier_classe == NULL){
-//         printf("le fichier n' a pas pu etre ouvert");
+    return 0;
+}
+
+
+int ajout_note(){
+    NOTE note;
+    printf("Veuillez saisir le numero de l'etudiant");
+    scanf("%d", &note.numero);
+    if (!numero_existe(note.numero)){
+        printf("Il n'existe pas d'etudiant avec ce numero");
+        return 1;
+    }
+    printf("Veuillez saisir la reference de la matiere ");
+    scanf ("%d", &note.reference);
+    if (!reference_existe(note.reference)){
+        printf("Il n'existe pas de matiere avec cette reference");
+        return 1;
+    }
+    printf("Veuillez saisir la note de cc ");
+    scanf ("%d", &note.noteCC);
+    printf("Veuillez saisir la note de ds ");
+    scanf ("%d", &note.noteDS);
+
+    FILE *fichier_note = fopen("note.csv", "a");
+    if (fichier_note == NULL){
+        printf("le fichier n' a pas pu etre ouvert");
        
-//         return 1;
-//     }
-//     if (level == 1)
-//         fprintf(fichier_classe,"\n%d - %s - License",classe.code, classe.nom);
-//     else
-//         fprintf(fichier_classe,"\n%d - %s - Master",classe.code, classe.nom);
-    
+        return 1;
+    }
+    fprintf(fichier_note, "%d - %d - %d - %d", note.numero, note.reference, note.noteCC, note.noteDS);
 
-//     fclose(fichier_classe);
-//     return 0;
-
+    fclose(fichier_note);
+    return 0;
 }
