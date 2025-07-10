2006259a89cd3d573781cd19c92aec6d016fed56
Big-Clo <claudeandiaye06@gmail.com>
2025-07-10 22:41:31 +0000

    matiere

--- matiere.h
+++ matiere.h
@@ -12,7 +12,10 @@ int ajout_matiere();
 int lister_matiere();
 char* chercher(int a,char *trouve);
 int supprimer_matiere(char line_sup[100]);
-int chercher_matiere();
+int rech_ref(int a);
+int rech_lib(char a[15]);
+int rech_coef(int a);
+void menuRecherche_matiere();
 int modifier_matiere(char line_modif[100]);
 
 #endif
