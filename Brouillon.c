c95364691eaf14b0284b6e3a39b1540deed11d4b
Ousmae Ladetail <dieyeousmanedieng@gmail.com>
2025-07-12 15:26:47 +0000

    voila

--- Brouillon.c
+++ Brouillon.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "matiere.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include "matiere.h"
 
 
 
 
 
-int main(){
-    menuRecherche();
+// int main(){
+//     menuRecherche();
     
-    return 0;
+//     return 0;
 
-}
+// }
